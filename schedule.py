04755825d1dc1e8ff80498627f2193b15d279965
Julie Tran <tranjulie@berkeley.edu>
2018-09-29 14:21:42 -0700

    Made scheduler

--- schedule.py
+++ schedule.py
@@ -1,4 +1,5 @@
 import argparse
+from random import randint
 parser = argparse.ArgumentParser()
 parser.add_argument('-input')
 parser.add_argument('-n')
@@ -6,8 +7,7 @@ args = parser.parse_args()
 print(args)
 
 class Match:
-    def __init__(self, num, b1, b2, g1, g2):
-        self.num =  num
+    def __init__(self, b1, b2, g1, g2):
         self.b1 = b1
         self.b2 = b2
         self.g1 = g1
@@ -17,19 +17,49 @@ class Team:
     def __init__(self, id, name):
         self.id = id
         self.name = name
-        self.lastmatch = 0
-        self.numPlayed = 0
+        self.lastMatch = -6
 
-schools = {}
-totalMatches = args.n
+schools = []
+totalRounds = int(args.n)
 
 def process(line):
     values = line.rstrip().split(",")
-    schools[values[0]] = Team(values[0], values[1])
+    schools.append(Team(values[0], values[1]))
 
 with open(args.input) as f:
     for line in f:
         process(line)
-print(schools)
 
-#for i in totalMatches:
+matches = []
+for i in range(totalRounds):
+    teamsLeft = schools.copy()
+    matchedTeams = []
+    alreadyPlayed = []
+    while len(teamsLeft) >= 4:
+        for i in range(4):
+            team = randint(0, len(teamsLeft) - 1)
+            while teamsLeft[team].lastMatch == len(matches) - 1:
+                team = randint(0, len(teamsLeft) - 1)
+            matchedTeams.append(teamsLeft[team])
+            alreadyPlayed.append(teamsLeft[team])
+            del teamsLeft[team]
+        matches.append(Match(matchedTeams[0], matchedTeams[1], matchedTeams[2], matchedTeams[3]))
+        for team in matchedTeams:
+            team.lastMatch = len(matches) - 1
+        matchedTeams = []
+    if len(teamsLeft) != 0:
+        matchedTeams.extend(teamsLeft)
+        for i in range(4 - len(teamsLeft)):
+            team = randint(0, len(alreadyPlayed) - 1)
+            while alreadyPlayed[team].lastMatch == len(matches) - 1:
+                team = randint(0, len(alreadyPlayed) - 1)
+            matchedTeams.append(alreadyPlayed[team])
+            del alreadyPlayed[team]
+        matches.append(Match(matchedTeams[0], matchedTeams[1], matchedTeams[2], matchedTeams[3]))
+        for team in matchedTeams:
+            team.lastMatch = len(matches) - 1
+        matchedTeams = []
+
+for m in matches:
+    m_str = m.b1.name + ", " + m.b2.name + ", " + m.g1.name + ", " + m.g2.name + ", "
+    print(m_str)
